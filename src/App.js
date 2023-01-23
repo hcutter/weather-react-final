b57a2abe0babd30340c7bdd8c75d1176a845ff40
hcutter <cutterh98@gmail.com>
2023-01-23 09:13:39 -0500

    Added a footer with a link to my GitHub

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project is created by Hannah Cutter
+        <a
+          href="https://github.com/hcutter/weather-react-final"
+          target="_blank"
+        ></a>
+        open sourced on GitHub
+      </footer>
+    </div>
+  );
 }
