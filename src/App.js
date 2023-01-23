1f3206acafb35c7040efbf04099776e5258fcc4f
hcutter <cutterh98@gmail.com>
2023-01-23 10:09:42 -0500

    HTML elements and bootstrap added to application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+      </div>
       <footer>
         This project is created by Hannah Cutter
         <a
