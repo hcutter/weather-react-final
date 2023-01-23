60c8f72c4e17203a8e63d609c6cd2dece5c7788a
hcutter <cutterh98@gmail.com>
2023-01-23 11:06:39 -0500

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -36,10 +36,8 @@ export default function Weather() {
               alt="Mostly Cloudy"
               className="float-left"
             />
-            <div className="float-left">
-              <span className="temperature">6</span>
-              <span className="unit">°C</span>
-            </div>
+            <span className="temperature">6</span>
+            <span className="unit">°C</span>
           </div>
         </div>
         <div className="col-6">
